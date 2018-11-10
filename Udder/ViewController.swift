e41e27715cb17230c15bf3063f57c0bba151d9ad
Aleksandra Konopka <aleksandrakonopka@MacBook-Air-Aleksandra.local>
2018-11-10 18:20:44 +0100

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Aleksandra Konopka on 10/11/2018.
+//  Copyright © 2018 Aleksandra Konopka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
